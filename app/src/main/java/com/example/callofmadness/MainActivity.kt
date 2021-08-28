5e960aeb74c2bd3a51e7a131428b6b0eb40efda8
JungSangHyeon <dev.jusa@gmail.com>
2021-08-28 21:26:45 +0900

    Main Function Refactoring

--- app/src/main/java/com/example/callofmadness/MainActivity.kt
+++ app/src/main/java/com/example/callofmadness/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.callofmadness
 
 import android.Manifest
-import android.content.DialogInterface
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
-import android.view.View
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.callofmadness.databinding.ActivityMainBinding
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,20 +24,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initView()
+        checkPermission()
+    }
+    private fun initView() {
         binding.numberSelectSeekBar.setOnSeekBarChangeListener(NumberBarChangeListener())
-        binding.callButton.setOnClickListener(CallButtonClickListener())
-
-        var initNumber = 88128140;
-        updateNumber(initNumber)
-        binding.numberSelectSeekBar.progress = initNumber
-
-        requestPermission();
+        binding.callButton.setOnClickListener {startCall()}
+        updateNumber(binding.numberSelectSeekBar.progress)
     }
 
-    inner class CallButtonClickListener : View.OnClickListener {
-        override fun onClick(v: View?) {
-            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:010" + String.format("%08d", binding.numberSelectSeekBar.progress))));
-        }
+    private fun startCall() {
+        val callUri = Uri.parse("tel:010" + String.format("%08d", binding.numberSelectSeekBar.progress))
+        val callIntent = Intent(Intent.ACTION_CALL, callUri)
+        startActivity(callIntent)
     }
     inner class NumberBarChangeListener : OnSeekBarChangeListener {
         override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {updateNumber(progress)}
@@ -49,54 +44,37 @@ class MainActivity : AppCompatActivity() {
         override fun onStopTrackingTouch(seekBar: SeekBar?) {}
     }
 
+    @SuppressLint("SetTextI18n")
     private fun updateNumber(progress: Int) {
         binding.nowSelectedNumberTextView.text = "010-"+String.format("%04d", progress/10000)+"-"+String.format("%04d", progress%10000);
     }
 
 
-    val PERMISSIONS_REQUEST_CODE = 100
-    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.CALL_PHONE)
-
-    private fun requestPermission(){
-        var permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+    private fun checkPermission(){
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
         if(permissionCheck != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
+            ActivityCompat.requestPermissions(this, arrayOf( Manifest.permission.CALL_PHONE), 0 )
         }
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            PERMISSIONS_REQUEST_CODE -> {
-                if(grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED){
-                    showRequestPermissionDialog();
-                }else{
-                }
-            }
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED){
+            showRequestPermissionDialog()
         }
     }
-
     private fun showRequestPermissionDialog(){
-        var builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(this)
         builder
             .setTitle("알림")
             .setMessage("권한이 없으면 사용할 수 없습니다. 권한을 설정해주세요.")
             .setCancelable(false)
-            .setPositiveButton("예") {dialog, which -> startPermissionSettingActivity()}
-            .setNegativeButton("아니오") {dialog, which -> endApplication()}
+            .setPositiveButton("예") { _, _ -> startPermissionSettingActivity()}
+            .setNegativeButton("아니오") { _, _ -> finish()}
         builder.create().show()
     }
-
-    private fun endApplication() {
-        finish()
-    }
-
     private fun startPermissionSettingActivity() {
-        var intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            .setData( Uri.parse("package:" + getPackageName()))
-        startActivity(intent);
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            .setData(Uri.parse("package:$packageName"))
+        startActivity(intent)
     }
-
 }
