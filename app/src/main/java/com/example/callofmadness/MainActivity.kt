4077811b0e914cab2607a3c4ad92f85a0e4ed83d
JungSangHyeon <dev.jusa@gmail.com>
2021-08-28 18:47:34 +0900

    Update Main View

--- app/src/main/java/com/example/callofmadness/MainActivity.kt
+++ app/src/main/java/com/example/callofmadness/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.callofmadness
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import com.example.callofmadness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.mainActivityNumberSelectSeekBar.setOnSeekBarChangeListener(NumberBarChangeListener())
+    }
+
+    inner class NumberBarChangeListener : OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            binding.mainActivityNowSelectedNumberTextView.text = "010-"+(progress/10000)+"-"+(progress%10000);
+        }
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {}
     }
 }
